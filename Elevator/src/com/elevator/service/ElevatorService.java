3a811c802bc13caa06a298949852c2a4b531ff47
lsh251 <lsh22511@gmail.com>
2022-12-27 13:04:15 +0900

    - main 메소드에 있던 코드를 다 옮김 - 변수명 수정, 중복되는 코드 메소드화함

--- Elevator/src/com/elevator/service/ElevatorService.java
+++ Elevator/src/com/elevator/service/ElevatorService.java
@@ -1,44 +1,49 @@
 package com.elevator.service;
 
 import java.util.Scanner;
-/*
- * *엘베
- * 엘베에 총 4명이 탑승 가능함
- * 엘베에 4개의 공간(배열)이 있는거야. 
- * 
- * 탑승할 때 엘베의 빈공간(-1)을 차지하고 목적층을 값으로 한다.
- * ex. 3층 선택한 경우 elevator[0] = 3;
- * 
- * *층수
- * 층수는 총 3개의 층
- * 
- * upAndDown이 false라면 내려가는거다.
- * 
- * - 층안내는 따로 메소드 만들자
- * - 근데 2차원배열로 해야하는거 아니냐? 엘베[3층][4명] 아 그럼 12명이지..
- */
+
 public class ElevatorService {
 	
-	int[] elevator = new int[4];
-	int count; // 탑승인원 카운트
-	
-	int[] floors = new int[3];
+	int[] elevator;
 	int curFloor; // 현재 층
-	
-	boolean upAndDown;
+	int passengerCount; // 탑승인원 카운트
+	boolean direction; // 위,아래 방향
 	
 	public ElevatorService() {
+		elevator = new int[4]; // 최대 4명까지 탑승 가능함.
 		for(int i=0; i<4; i++)
 			elevator[i] = -1;
 		
-		count = 0;
-		
 		curFloor = 0;
-		
-		upAndDown = true;
+		passengerCount = 0;
+		direction = true;
 	}
 	
-
+	public void start() {
+		System.out.println("---- Welcome Elevator ----");
+		
+		EXIT:
+		while(true) {
+			
+			int menu = inputMenu();
+			
+			switch(menu) {
+			case 1:
+				join();
+				break;
+			case 2:
+				move();
+				break;
+			case 3:
+				status();
+				break;
+			case 4:
+				System.out.println("엘리베이터 운행을 종료합니다.");
+				break EXIT;
+			}
+		}
+	}
+	
 	public int inputMenu() {
 		int menu;
 		
@@ -53,11 +58,11 @@ public class ElevatorService {
 	}
 
 	public void join() {
-		int floor;
+		int answer;
 		
 		Scanner scan = new Scanner(System.in);
 		
-		if(count==4) {
+		if(passengerCount==4) {
 			System.out.println("탑승할 공간이 없습니다.");
 			return;
 		}
@@ -66,62 +71,64 @@ public class ElevatorService {
 				System.out.println("층 수를 선택해 주세요.");
 				System.out.println("1.1층 2.2층 3.3층");
 				System.out.print(">");
-				floor = scan.nextInt()-1;
+				answer = scan.nextInt()-1;
 				
 				
-				if(curFloor==floor)
-					System.out.println("목적지로 현재 층은 안됩니다.");
+				if(answer<0 || 2<answer)
+					System.out.println("운행 범위를 벗어났습니다.");
+				else if(curFloor==answer)
+					System.out.printf("현재 %d층입니다.\n", answer+1);
 				else {
-					if(upAndDown) {
-						if(curFloor>floor) // upAndDown true인 경우 현재층보다 낮은층 고를 수 없어
+					if(direction) {
+						if(curFloor>answer) // direction true인 경우 현재층보다 낮은층 고를 수 없어
 							System.out.println("올라갑니다."); 
 					} else {
-						if(curFloor<floor) // upAndDown flase인 경우 현재층보다 높은층 고를 수 없어
+						if(curFloor<answer) // direction flase인 경우 현재층보다 높은층 고를 수 없어
 							System.out.println("내려갑니다."); 
 					}
 				}
 				
-			} while(curFloor==floor || (upAndDown&&curFloor>floor) || (!upAndDown&&curFloor<floor));
+			} while((answer<0 || 2<answer) || curFloor==answer 
+					|| (direction&&curFloor>answer) || (!direction&&curFloor<answer));
 			
 			// 입력 층 안내
-			System.out.printf("입력하신 층은 %d층입니다.\n", floor+1);
+			System.out.printf("입력하신 층은 %d층입니다.\n", answer+1);
+			
 			// 탑승 시키기 
 			for(int i=0; i<4; i++) // 4번 반복해서 -1이 있다면 해당 자리에 탑승 시키고 break 한다.
 				if(elevator[i] == -1) {
-					elevator[i] = floor;
-					count++;
+					elevator[i] = answer;
+					passengerCount++; // 탑승인원 카운트를 증가시킨다.
 					break;
 				} 
 		}
-		
 	}
 
 	public void move() {
 		// 엘베를 이동시킨다.
-		if(upAndDown) {
+		if(direction) {
 			curFloor++;
 			if(curFloor==2)
-				upAndDown = false;
+				direction = false; // 최상층에서 방향을 바꾼다.
 		}
 		else {
 			curFloor--;
 			if(curFloor==0)
-				upAndDown = true;
+				direction = true; // 최하층에서 방향을 바꾼다.
 		}
 		
 		// 현재 층을 안내한다.
-		System.out.printf("현재 층수는 %d층 입니다.\n", curFloor+1);
+		infoFloor();
 		
 		// 하차 안내한다. 
-		int num = 0;
+		int count = 0;
 		for(int i=0; i<4; i++)
-			if(curFloor==elevator[i]) {
+			if(curFloor==elevator[i]) { // 목적지가 현재층인 경우
 				elevator[i] = -1;
-				count--;
-				num++;
+				passengerCount--;
+				count++;
 			}
-				
-		System.out.printf("%d명이 하차하였습니다.", num);
+		System.out.printf("%d명이 하차하였습니다.\n", count);
 	}
 
 
@@ -129,19 +136,19 @@ public class ElevatorService {
 		System.out.println("---- 탑승 현황 ----");
 		
 		// 탑승 인원을 안내한다.
-		// 값이 -1이 아닌 경우 탑승인원이 있는 것이다.
 		int count = 0;
 		for(int i=0; i<4; i++)
-			if(elevator[i] != -1)
+			if(elevator[i] != -1) // 값이 -1이 아닌 경우 탑승인원이 있는 것이다.
 				count++;
 		System.out.printf("현재 탑승 인원은 %d명 입니다.\n", count);
 		
 		// 현재 층을 안내한다.
-		System.out.printf("현재 층수는 %d층 입니다.\n", curFloor+1);
+		infoFloor();
 	}
 
-
-
+	public void infoFloor() {
+		System.out.printf("현재 층수는 %d층 입니다.\n", curFloor+1);
+	}
 }
 
 
